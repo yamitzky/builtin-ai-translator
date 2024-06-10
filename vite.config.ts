07a742098ec6414052a2321545857a4040b89e19
Mitsuki Ogasahara <negiga@gmail.com>
2024-06-11 01:37:34 +0900

    Add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: "auto",
+			workbox: {
+				globPatterns: ["**/*.{js,css,html}"],
+			},
+			manifest: {
+				name: "Built-in AI Translator",
+				short_name: "Built-in AI Translator",
+				description: "AI Translator using Gemini Nano",
+				theme_color: "#0E324E",
+				background_color: "#fff",
+				display: "standalone",
+				icons: [
+					{
+						src: "/icon-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/icon-256x256.png",
+						sizes: "256x256",
+						type: "image/png",
+					},
+					{
+						src: "/icon-384x384.png",
+						sizes: "384x384",
+						type: "image/png",
+					},
+					{
+						src: "/icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
+});
